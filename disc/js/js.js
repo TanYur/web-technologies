e68d46f39f29c63c338072af3c598d3db929b3bd
TanaYur <8549tan.y@gmail.com>
2023-05-02 15:40:43 +0200

    add lang

--- disc/js/js.js
+++ disc/js/js.js
@@ -1,43 +1,55 @@
+//lang choise
+const lang = document.querySelectorAll('.lang button.lang-choise');
+const langMark = document.querySelectorAll('.lang-mark');
+
+lang.forEach(i => {
+    i.addEventListener('click', function () {
+        let arr = Array.from(langMark);
+        if (arr.some(j=>j.classList.contains('visible'))) {
+            langMark.forEach(x => x.classList.remove('visible'));
+        } else langMark.forEach(x => x.classList.add('visible'));
+    });
+});
+
+
 //show menu-burger in mobile view
 const menuburger = document.getElementById('menuburger');
 const menu = document.querySelector('.menu-main-mobile');
-const menuBg = document.querySelector('.menu-bg');
 
 
 menuburger.addEventListener("click", showMenu);
 function showMenu() {
-    if (menu.classList.contains('menu-main-mobile-visible')) {
-        menu.classList.remove('menu-main-mobile-visible');
-        menuBg.classList.remove('menu-bg-visible');
+    if (menu.classList.contains('visible')) {
+        menu.classList.remove('visible');
     } else {
-        menu.classList.add('menu-main-mobile-visible');
-        menuBg.classList.add('menu-bg-visible');
+        menu.classList.add('visible');
     }
 }
 
 menu.addEventListener('click', hideMenu);
 function hideMenu() {
-    menu.classList.remove('menu-main-mobile-visible');
-    menuBg.classList.remove('menu-bg-visible');
+    menu.classList.remove('visible');
 }
 
 //hover effect
 const gridItems = document.querySelectorAll('.grid-icon-item');
 const stagesHover = document.querySelectorAll('.stages-hover');
 
-gridItems.forEach((el, i) => {
+gridItems.forEach((el) => {
     let item = el.querySelector('.stages-hover');
-       
-    el.addEventListener('mouseover', function(){
-        item.classList.add('stages-hover-visible');
+
+    el.addEventListener('mouseover', function () {
+        item.classList.add('visible');
     });
-    el.addEventListener('mouseleave', function(){
-        item.classList.remove('stages-hover-visible');
+    el.addEventListener('mouseleave', function () {
+        item.classList.remove('visible');
     });
-    el.addEventListener('touchstart', function(){
-        item.classList.add('stages-hover-visible');
+    el.addEventListener('touchstart', function () {
+        item.classList.add('visible');
     });
-    el.addEventListener('touchend', function(){
-        item.classList.remove('stages-hover-visible');
+    el.addEventListener('touchend', function () {
+        item.classList.remove('visible');
     });
 });
+
+
